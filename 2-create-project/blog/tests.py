eb403dc72917d07a146b53c9e1cc9879407a9c61
jmhwang-dev <jungmin.hwang.dev@gmail.com>
2024-03-12 22:30:38 +0900

    테스트코드 작성 예제 마무리

--- 2-create-project/blog/tests.py
+++ 2-create-project/blog/tests.py
@@ -9,45 +9,49 @@ class TestView(TestCase):
         self.client = Client()
 
     def test_post_list(self):
-        # 1.1
+        # 1.1 포스트 목록 페이지를 가져온다.
         response = self.client.get('/blog/')
 
-        # 1.2
+        # 1.2 정상적으로 페이지가 로드된다.
         self.assertEqual(response.status_code, 200)
 
-        # 1.3
+        # 1.3 페이지 타이틀은 'Blog'이다.
         soup = BeautifulSoup(response.content, 'html.parser')
         self.assertEqual(soup.title.text, 'Blog')
 
-        # 1.4
+        # 1.4 내비게이션 바가 있다.
         navbar = soup.nav
         
-        # 1.5
+        # 1.5 Blog, About Me라는 문구가 내비게이션 바에 있다.
         self.assertIn("Blog", navbar.text)
         self.assertIn('About Me', navbar.text)
 
-        # 2.1
+        # 2.1 게시물(포스트)가 하나도 없다면
         self.assertEqual(Post.objects.count(), 0)
+        # main-area에 '아직 게시물이 없습니다.' 라는 문구가 나타난다.
         main_area = soup.find('div', id="main-area")
         self.assertIn('아직 게시물이 없습니다.', main_area.text)
 
-        # post_001 = Post.objects.create(
-        #     title="첫 번째 포스트입니다.",
-        #     content="Hello World. We are the world.",
-        # )
-        # post_002 = Post.objects.create(
-        #     title="두 번째 포스트입니다.",
-        #     content="1등이 전부는 아니잖아요?",
-        # )
-        # self.assertEqual(Post.objects.count(), 2)
+        # 3.1 포스트가 2개가 만들었을 때,
+        post_001 = Post.objects.create(
+            title="첫 번째 포스트입니다.",
+            content="Hello World. We are the world.",
+        )
+        post_002 = Post.objects.create(
+            title="두 번째 포스트입니다.",
+            content="1등이 전부는 아니잖아요?",
+        )
+        self.assertEqual(Post.objects.count(), 2)
 
-        # #
-        # response = self.client.get('/blog/')
-        # soup = BeautifulSoup(response.content, 'html.parser')
-        # self.assertEqual(response.status_code, 200)
+        # 3.2 포스트 목록을 새로고침 하면
+        response = self.client.get('/blog/')
+        soup = BeautifulSoup(response.content, 'html.parser')
+        self.assertEqual(response.status_code, 200)
 
-        # main_area = soup.find('div', id='main-area')
-        # self.assertIn(post_001.title, main_area.text)
-        # self.assertIn(post_002.title, main_area.text)
+        # 3.3 main-area에 포스트 2개의 제목이 존재하고
+        main_area = soup.find('div', id='main-area')
+        self.assertIn(post_001.title, main_area.text)
+        self.assertIn(post_002.title, main_area.text)
 
-        # self.assertNotIn('아직 게시물이 없습니다.', main_area.text)
+        # 3.4 '아직 게시물이 없습니다.' 라는 문구는 더이상 나타나지 않는다.
+        self.assertNotIn('아직 게시물이 없습니다.', main_area.text)
