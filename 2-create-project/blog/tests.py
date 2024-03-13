54f2b26c15fb3408e690e0c10cecda2fcc9d2889
jmhwang-dev <jungmin.hwang.dev@gmail.com>
2024-03-13 22:56:02 +0900

    errata

--- 2-create-project/blog/tests.py
+++ 2-create-project/blog/tests.py
@@ -73,7 +73,7 @@ class TestView(TestCase):
         # 2.2 포스트 목록 페이지와 똑같은 내비게이션 bar가 있다.
         soup = BeautifulSoup(response.content, 'html.parser')
         navbar = soup.nav
-        self.assertIn('Blog', navbar.txt)
+        self.assertIn('Blog', navbar.text)
         self.assertIn("About Me", navbar.text)
 
         # 2.3 첫 번째 포스트의 제목이 웹 브라우저 탭 타이틀에 들어 있다.
