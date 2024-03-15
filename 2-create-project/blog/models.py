0b18a3b2c89a034ba018272f7789b665b8529211
jmhwang-dev <jungmin.hwang.dev@gmail.com>
2024-03-15 15:12:18 +0900

    Category 모델 만들기: SlugField 사용

--- 2-create-project/blog/models.py
+++ 2-create-project/blog/models.py
@@ -4,6 +4,14 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=50, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
 class Post(models.Model):
     title = models.CharField(max_length=30)
     hook_text = models.CharField(max_length=100, blank=True)
