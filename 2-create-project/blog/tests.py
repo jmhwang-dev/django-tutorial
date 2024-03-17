e63b9ff1a2f1477b80d8ed22c01bb7a28044f4bd
jmhwang-dev <jungmin.hwang.dev@gmail.com>
2024-03-17 21:30:07 +0900

    카테고리 페이지 만들기
    1. test 코드에서 url 확인 추가
    2. Category 모델에 url 반환 메소드 추가

--- 2-create-project/blog/tests.py
+++ 2-create-project/blog/tests.py
@@ -125,3 +125,7 @@ class TestView(TestCase):
         self.assertIn(self.post_001.content, post_area.text)
 
         self.assertIn(self.user_trump.username.upper(), post_area.text)
+
+    def test_category_page(self,):
+        response = self.client.get(self.category_programming.get_absolute_url())
+        self.assertEqual(response.status_code, 200)
