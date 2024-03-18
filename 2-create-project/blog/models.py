77da0caf57fa0c54e6c91135a97de77738a2b0c0
jmhwang-dev <jungmin.hwang.dev@gmail.com>
2024-03-18 14:17:54 +0900

    Tag 모델 추가
    - tags 필드를 빈 칸으로 남겨둘 수 있도록 null=True, blank=True
    - on_delete=models.SET_NULL은 설정하지 않는다
      -> 연결된 태그가 삭제되면 해당 포스트의 tags 필드는 알아서 빈 칸으로 바뀌기 때문

--- 2-create-project/blog/models.py
+++ 2-create-project/blog/models.py
@@ -4,6 +4,15 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self,):
+        return f'/blog/tag/{self.slug}/'
 
 class Category(models.Model):
     name = models.CharField(max_length=50, unique=True)
@@ -30,6 +39,7 @@ class Post(models.Model):
     
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
+    tags = models.ManyToManyField(Tag, null=True, blank=True)
 
     def __str__(self) -> str:
         return f'[{self.pk}]{self.title} :: {self.author}'
