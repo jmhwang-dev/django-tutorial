db50e86495e9dc246259d2eebb81e7aba4dd8e05
jmhwang-dev <jungmin.hwang.dev@gmail.com>
2024-03-20 16:12:21 +0900

    post_form.html의 테스트 코드 추가

--- 2-create-project/blog/tests.py
+++ 2-create-project/blog/tests.py
@@ -182,3 +182,9 @@ class TestView(TestCase):
     def test_create_page(self,):
         response = self.client.get('/blog/create_post/')
         self.assertEqual(response.status_code, 200)
+
+        soup = BeautifulSoup(response.content, 'html.parser')
+        self.assertEqual('Create Post - Blog', soup.title.text)
+        
+        main_area = soup.find('div', id='main-area')
+        self.assertIn('Create New Post', main_area.text)
