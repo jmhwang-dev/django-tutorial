7fccdce78dd6c5055e30b6daca54c7754c2bc9a3
jmhwang-dev <jungmin.hwang.dev@gmail.com>
2024-03-21 22:42:07 +0900

    포스트 작성자만 수정할 수 있게 구현하기 - 템플릿 지정
    PostUpdate는 UpdateView 상속받은 클래스다.
    CreateView, UpdateView는 모델명 뒤에 '_form.html'이 붙은 템플릿 파일을 기본으로 사용하도록 설정되어있다.
    그래서 PostUpdate에서도 PostCreate를 작성할 때 만들어둔 post_form.html을 사용한 것.
    여기에서는 template_name을 따로 지정해서 템플릿 파일을 따로 지정하도록한다.

--- 2-create-project/blog/tests.py
+++ 2-create-project/blog/tests.py
@@ -239,3 +239,24 @@ class TestView(TestCase):
         )
         response = self.client.get(update_post_url)
         self.assertEqual(response.status_code, 200)
+
+        soup = BeautifulSoup(response.content, 'html.parser')
+        self.assertEqual('Edit Post - Blog', soup.title.text)
+        main_area = soup.find('div', id='main-area')
+        self.assertIn('Edit Post', main_area.text)
+
+        response = self.client.post(
+            update_post_url,
+            {
+                'title': '세 번째 포스트를 수정했습니다.',
+                'content': "안녕 세계? 우리는 하나!",
+                'category': self.category_music.pk
+            },
+            follow=True
+        )
+
+        soup = BeautifulSoup(response.content, 'html.parser')
+        main_area = soup.find('div', id='main-area')
+        self.assertIn('세 번째 포스트를 수정했습니다.', main_area.text)
+        self.assertIn("안녕 세계? 우리는 하나!", main_area.text)
+        self.assertIn(self.category_music.name , main_area.text)
--- 2-create-project/blog/views.py
+++ 2-create-project/blog/views.py
@@ -26,6 +26,7 @@ class PostCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
 class PostUpdate(LoginRequiredMixin, UpdateView):
     model = Post
     fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category', 'tags']
+    template_name = 'blog/post_update_form.html'
 
     def dispatch(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
         if request.user.is_authenticated and request.user == self.get_object().author:
