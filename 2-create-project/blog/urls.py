6512e50b38c7cac951cddf61560e5f68c0d6bb3a
jmhwang-dev <jungmin.hwang.dev@gmail.com>
2024-04-03 17:54:11 +0900

    docker에 gunicorn 적용하기

--- 2-create-project/blog/urls.py
+++ 2-create-project/blog/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('search/<str:q>/', views.PostSearch.as_view()),
@@ -13,3 +16,6 @@ urlpatterns = [
     path('<int:pk>/', views.PostDetail.as_view()),
     path('', views.PostList.as_view())
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- 2-create-project/do_it_django_prj/settings.py
+++ 2-create-project/do_it_django_prj/settings.py
@@ -155,9 +155,11 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '_static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
+
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
