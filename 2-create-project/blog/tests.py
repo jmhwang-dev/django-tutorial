852f47d148705d9c1a9bc8793c76aeaeb3a9acb4
jmhwang-dev <jungmin.hwang.dev@gmail.com>
2024-04-09 14:08:47 +0900

    erratum

--- 2-create-project/blog/tests.py
+++ 2-create-project/blog/tests.py
@@ -214,7 +214,7 @@ class TestView(TestCase):
         main_area = soup.find('div', id='main-area')
         self.assertIn('Create New Post', main_area.text)
 
-        tag_str_input = main_area.find('input', id="id_tags_str")
+        tag_str_input = main_area.find('input', id="id_tgas_str")
         self.assertTrue(tag_str_input)
 
         self.client.post(
